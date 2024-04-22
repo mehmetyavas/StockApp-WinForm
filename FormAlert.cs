698ef4a4b66e2a0d5d3da71c818c32e22216e26a
Mehmet Emin Yavaş <mehmett_175@outlook.com>
2024-04-22 17:59:24 +0300

    alert

--- FormAlert.cs
+++ FormAlert.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using StockApp.Properties;
-namespace URETIM
+namespace StockApp
 {
     public partial class FormAlert : Form
     {
--- FormAlert.designer.cs
+++ FormAlert.designer.cs
@@ -1,4 +1,4 @@
-﻿namespace URETIM
+﻿namespace StockApp
 {
     partial class FormAlert
     {
