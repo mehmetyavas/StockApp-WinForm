d5430fa7e032d5eb3dc0f6eb99ea7ac664e16822
Mehmet Emin Yavaş <mehmett_175@outlook.com>
2024-04-22 17:40:50 +0300

    prdGrid

---
+++ Data/Entity/GridEntity/ProductGrid.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Data.Entity
+{
+    public class ProductGrid:Product
+    {
+        public int Adet => 1;
+    }
+}
